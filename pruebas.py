4b7316f27e68ef8defda38e3061318167e2d1379
Carlos Daniel Jaimes Retiguin <cjaimesretiguin@gmail.com>
2020-06-05 13:33:59 -0500

    Clase 8. Definir funciones con Python - mksquere w/ turtle and functions

--- pruebas.py
+++ pruebas.py
@@ -1,2 +1,29 @@
-name = str(input("¿Cuál es tu nomre? "))
-print("Hola, " + name)
+import turtle
+
+def main(): #Esta será la función principal
+    window = turtle.Screen() #Abrimos la ventana
+    tortuga = turtle.Turtle() #Generar una tortuga
+
+    mksquare(tortuga) #Haz un cuadrado
+    turtle.mainloop() #Mantener la ventana abierta
+
+def mksquare(tortuga):
+    length = int(input("Qué tan grande quieres tu cuadrado: "))
+    
+    for i in range(4):
+        mklineAndTurn(tortuga, length)
+
+def mklineAndTurn(tortuga, length):
+    tortuga.forward(length)
+    tortuga.left(90)
+    
+
+
+""" 
+
+Es importante definir dónde tienen que empezar los programas en python, pues son una secuencia de comandos y tienen que iniciar en algún lugar. Python le llama main a los archivos que corre.
+
+"""
+
+if __name__ == "__main__": #Si el nombre de este archivo es igual al main que estás corriendo, ejecuta la función main
+    main()
