7c0373d1b03531ac8c3fe34dfa41e74d6e0ae848
Carlos Daniel Jaimes Retiguin <cjaimesretiguin@gmail.com>
2020-06-06 17:34:13 -0500

    Clase 23 y 24: Juego del ahorcado. Módulo os. Aporte

--- pruebas.py
+++ pruebas.py
@@ -1,8 +1,10 @@
-import random
+import random 
+import os
 
+#declaramos una constante con las diferentes posiciones del ahorcado
 
-
-IMAGES = ['''
+IMAGES = [
+'''
 
     +---+
     |   |
@@ -67,69 +69,79 @@ IMAGES = ['''
     |   |
    / \  |
         =========''', '''
-''']
+'''
 
-WORDS = [
-    'lavadora',
-    'secadora',
-    'sofa',
-    'gobierno',
-    'diputado',
-    'democracia',
-    'computadora',
-    'teclado'
 ]
 
 
+WORDS = [ 'secadora','moto','mariposa','computadora' ]   
+
 def random_word():
-    idx = random.randint(0, len(WORDS) - 1)
-    return WORDS[idx]
+    word = WORDS[random.randrange(len(WORDS))]
+    return word
 
-
-def display_board(hidden_word, tries):
+def display_board(hidden_word,tries):
     print(IMAGES[tries])
-    print('')
-    print(hidden_word)
-    print('--- * --- * --- * --- * --- * --- ')
-
+    print (hidden_word)
 
 def run():
     word = random_word()
-    hidden_word = ['-'] * len(word)
+    hidden_word=[' __ ']*len(word)
     tries = 0
 
     while True:
-        display_board(hidden_word, tries)
-        current_letter = str(input('Escoge una letra: '))
+        display_board(hidden_word,tries)
+        current_letter = str(input('ingresa la letra : '))
 
-        letter_indexes = []
+        index =[]
         for idx in range(len(word)):
-            if word[idx] == current_letter:
-                letter_indexes.append(idx)
+            if current_letter == word[idx]:
+                index.append(idx) 
+                # print(index) => Muestra los index donde estan las letras
 
-        if len(letter_indexes) == 0:
-            tries += 1
-
+        if len(index)==0 or hidden_word[index[0]]!=' __ ':
+            tries+=1
             if tries == 7:
-                display_board(hidden_word, tries)
-                print('')
-                print('¡Perdiste! La palabra correcta era {}'.format(word))
+                display_board(hidden_word,tries)
+                print('\n')
+                print('¡Perdiste! La palabra escondida es {}'.format(word))
                 break
+
         else:
-            for idx in letter_indexes:
-                hidden_word[idx] = current_letter
-
-            letter_indexes = []
-
+            for i in range(len(index)):
+                hidden_word[index[i]]=current_letter
+           
         try:
-            hidden_word.index('-')
+            hidden_word.index(' __ ')
         except ValueError:
-            print('')
-            print('¡Felicidades! Ganaste. La palabra es: {}'.format(word))
+            display_board(hidden_word,tries)
+            print('\n')
+            print('¡Felicidades!!! ganaste')
             break
 
+        os.system('cls')
+       
+        
+if __name__=='__main__':
 
+    print(''' 
 
-if __name__ == '__main__':
-    print('B I E N V E N I D O S  A  A H O R C A D O S')
+  ____  _                           _     _       
+ |  _ \(_)                         (_)   | |      
+ | |_) |_  ___ _ ____   _____ _ __  _  __| | ___  
+ |  _ <| |/ _ \ '_ \ \ / / _ \ '_ \| |/ _` |/ _ \ 
+ | |_) | |  __/ | | \ V /  __/ | | | | (_| | (_) |
+ |____/|_|\___|_| |_|\_/ \___|_| |_|_|\__,_|\___/ 
+                                                  
+               +---+
+           |   |
+           o
+          /|\  |
+           |   |
+          / \  |
+          =========                                         
+*************************************************
+\n \n \n
+ ''')
+  
     run()
