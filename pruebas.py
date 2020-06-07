f74dd7fc3816a1f50648848dd30ab7f6cf64c309
Carlos Daniel Jaimes Retiguin <cjaimesretiguin@gmail.com>
2020-06-07 00:58:17 -0500

    Clase 34. Errores personalizados - Manejo de errores con try, except - Cantidad de habitantes de algunos paises en millones

--- pruebas.py
+++ pruebas.py
@@ -1,38 +1,15 @@
-"""
-"abacabad" c
-"abacabaabacaba" _
-"abcdefghijklmnopqrstuvwxyziflskecznslkjfabe" d
-"bcccccccccccccyb" y
-"""
+countries = {
+    'mexico': 122,
+    'colombia': 49,
+    'argentina': 43,
+    'chile': 18,
+    'peru': 31
+}
 
-def first_not_repeating_char(char_sequence):
-    seen_letters = {}
+while True:
+    country = str(input('Escribe el nombre de un país: ')).lower()
 
-    for idx, letter in enumerate(char_sequence):
-        if letter not in seen_letters:
-            seen_letters[letter] = (idx, 1)
-        else:
-            seen_letters[letter] = (seen_letters[letter][0], seen_letters[letter][1] + 1)
-
-    final_letters = []
-    for key, value in seen_letters.items():
-        if value[1] == 1:
-            final_letters.append( (key, value[0]) )
-
-    not_repeated_letters = sorted(final_letters, key=lambda value: value[1])
-
-    if not_repeated_letters:
-        return not_repeated_letters[0][0]
-    else:
-        return '_'
-
-
-if __name__ == '__main__':
-    char_sequence = str(input('Escribe una secuencia de caracteres: '))
-
-    result = first_not_repeating_char(char_sequence)
-
-    if result == '_':
-        print('Todos los caracteres se repiten.')
-    else:
-        print('El primer caracter no repetido es: {}'.format(result))
+    try:
+        print('La población de {} es: {} millones'.format(country, countries[country]))
+    except KeyError:
+        print('No tenemos el dato de la población de {}'.format(country))
