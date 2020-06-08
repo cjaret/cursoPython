00ddab5220d49b91136347d787239dcb248a0c06
Carlos Daniel Jaimes Retiguin <cjaimesretiguin@gmail.com>
2020-06-08 01:35:54 -0500

    Clase 47. Creando templates con Jinja2 - carpeta Servidor

--- Servidor/main.py
+++ Servidor/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template #render_template nos va a permitir renderizar archivos html
 
 app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
-    return "Hola, little Charlie"
+    return render_template('contact_book.html')
 
 if __name__ == '__main__':
     app.run()
